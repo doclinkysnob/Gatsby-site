6b804aab6474158fbf61bc26caff785df32a5826
docmolotov <doclinkysnob@gmail.om>
2021-09-01 10:07:10 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Laundromatic",
+  },
+  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-gatsby-cloud"],
+};
